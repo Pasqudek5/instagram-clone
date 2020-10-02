e05a861b55f88f2a3d73bb3e9575beb3ab2f4276
= <=>
2020-10-02 18:55:15 +0200

    Add simple Protal component

---
+++ app/components/Portal/index.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * Portal
+ *
+ */
+
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
+const Portal = ({ children }) => {
+  const mount = document.getElementById('portal-root');
+  const el = document.createElement('div');
+
+  useEffect(() => {
+    mount.appendChild(el);
+    return () => mount.removeChild(el);
+  }, [el, mount]);
+
+  return createPortal(children, el);
+};
+
+Portal.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Portal;
